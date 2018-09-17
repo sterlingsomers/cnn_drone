aef855c9b569302ca9757dccb864f76b47ab1b4e
sterlingsomers <psomers@andrew.cmu.edu>
2018-09-17 13:51:25 -0400

    change to 20x20

--- gym_gridworld/envs/gridworld_env.py
+++ gym_gridworld/envs/gridworld_env.py
@@ -61,14 +61,16 @@ class GridworldEnv(gym.Env):
         self.dropping = True # This is for the reset to select the proper starting locations for hiker and drone
         self.restart_once_done = True  # restart or not once done
         self.drop = False
-        self.maps = [(390,50)]#[(400,35), (350,90), (430,110),(390,50), (230,70)] #[(86, 266)] (70,50) # For testing, 70,50 there is no where to drop in the whole map
+        self.maps = [(171,323)]#[(400,35), (350,90), (430,110),(390,50), (230,70)] #[(86, 266)] (70,50) # For testing, 70,50 there is no where to drop in the whole map
+        self.mapw = 20
+        self.maph = 20
         self.dist_old = 1000
         self.drop_package_grid_size_by_alt = {1: 3, 2: 5, 3: 7}
         self.factor = 5
         self.reward = 0
         self.action_space = spaces.Discrete(16)
         self.actions = list(range(self.action_space.n))
-        self.obs_shape = [50,50,3]
+        self.obs_shape = [100,100,3]
         self.observation_space = spaces.Box(low=0, high=255, shape=self.obs_shape)
         self.real_actions = False
 
@@ -565,7 +567,7 @@ class GridworldEnv(gym.Env):
         #Random generated map
         start = random.choice([1,1,1,1,1,1,1,1,1,1])
         stop = random.choice([4,4,4,4,4,4,4,4,4,4])
-        self.map_volume = CNP.create_custom_map(np.random.random_integers(start,stop,(10,10))) #CNP.create_custom_map(random.choice(self.custom_maps))
+        self.map_volume = CNP.map_to_volume_dict(_map[0],_map[1], self.mapw, self.maph)#CNP.create_custom_map(np.random.random_integers(start,stop,(self.mapw,self.maph))) #CNP.create_custom_map(random.choice(self.custom_maps))
         # Set hiker's and drone's locations
         #hiker = (random.randint(2, self.map_volume['vol'].shape[1] - 1), random.randint(2, self.map_volume['vol'].shape[1] - 2)) #(8,8) #
         #if self.dropping:
@@ -684,7 +686,7 @@ class GridworldEnv(gym.Env):
         #         canvas[hiker_point[0] * self.factor + point[0], hiker_point[1] * self.factor + point[1], :] = \
         #             self.map_volume['feature_value_map']['hiker']['color']
 
-        return imresize(np.flip(canvas, 0), 200, interp='nearest')
+        return imresize(np.flip(canvas, 0), 20*self.map_volume['vol'].shape[2], interp='nearest')
 
     def generate_observation(self):
         obs = {}
--- run_agent.py
+++ run_agent.py
@@ -42,7 +42,7 @@ flags.DEFINE_integer("all_summary_freq", 50, "Record all summaries every n batch
 flags.DEFINE_integer("scalar_summary_freq", 5, "Record scalar summaries every n batch")
 flags.DEFINE_string("checkpoint_path", "_files/models", "Path for agent checkpoints")
 flags.DEFINE_string("summary_path", "_files/summaries", "Path for tensorboard summaries")
-flags.DEFINE_string("model_name", "Drop_rand_positive_reward_reducedsteppenalty_distancereward_edgefix", "Name for checkpoints and tensorboard summaries")
+flags.DEFINE_string("model_name", "Drop_2020", "Name for checkpoints and tensorboard summaries")
 flags.DEFINE_integer("K_batches", 100000, # Batch is like a training epoch!
     "Number of training batches to run in thousands, use -1 to run forever") #(MINE) not for now
 flags.DEFINE_string("map_name", "DefeatRoaches", "Name of a map to use.")
